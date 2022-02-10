92549fedbdf0dbd2c4fc533d302a6c6f45b6f8e8
Daniel Paulino da Silva Falcao <daniel.falcao@havan.com.br>
2022-02-10 17:40:46 -0300

    Tudo junto em App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const App = () => {
+    //Criação da lista todos e do método settodos
+    const [todos, setTodos] = useState([
+        {id: 1, title: 'Ir ao mercado', done: false},
+        {id: 2, title: 'Ir ao cinema', done: false},
+        {id: 3, title: 'Ir ao shopping', done: false},
+    ]);
+    const [todo, setTodo] = useState();
+
+    //Manipula o submite do formulério
+    const handleFormSubmit = e => {
+        e.preventDefault();
+        setTodos([
+            ...todos,
+            todo,
+        ]);
+    }
+
+    //disparada toda vez que um campo do formulário for alterado
+    const handleInputChange = e => {        
+        setTodo({
+            ...todo,
+            id: todos.length + 1,
+            title: e.target.value,
+            done: false,
+        })
+        console.log(todo);
+    }
+
     return(
-    <div>
-        Meu App
-    </div>
+        <div>
+            {
+                todos.map(i => (
+                    <div key={i.id}>{i.title}</div>
+                ))
+            }        
+            <br />
+            <br />
+            <br />
+            <form onSubmit={handleFormSubmit}>
+                <input type="text" name="todo" 
+                    id="title" 
+                    placeholder="Nova tarefa..."
+                    onChange={handleInputChange} />
+                <button>ADICIONAR</button>
+            </form>
+        </div>
     );
 }
 
